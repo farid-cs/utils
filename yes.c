c4dd81fea4f5e5f0cdf90081774d7a7867304639
Farid <faridfarajli86@gmail.com>
2025-05-05 13:52:18 +0400

    simplify yes command

--- yes.c
+++ yes.c
@@ -3,12 +3,10 @@
 int
 main(int argc, char **argv)
 {
-	if (argc < 2)
-		for (;;)
-			printf("y\n");
-	for (;;) {
-		for (int i = 1; i != argc - 1; i++)
-			printf("%s ", argv[i]);
-		printf("%s\n", argv[argc-1]);
-	}
+	const char *s = "y";
+
+	if (argc > 1)
+		s = argv[1];
+	for (;;)
+		printf("%s\n", s);
 }
