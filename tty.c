1f1130d20e9cf5ed9de201339e08ee061809d96c
Farid <faridfarajli86@gmail.com>
2025-12-31 20:20:12 +0400

    feat(tty): handle ENOTTY

--- tty.c
+++ tty.c
@@ -1,20 +1,38 @@
 #include <assert.h>
 #include <errno.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
-int
-main(void)
+enum {
+	ExitSuccess,
+	ExitNotTty,
+	ExitFailure,
+};
+
+static char *
+parse_arguments(int argc, char **argv)
 {
+	assert(argc > 0);
+	assert(argv != nullptr);
+	return argv[0];
+}
+
+int
+main(int argc, char **argv)
+{
+	const char *argv0 = parse_arguments(argc, argv);
 	const char *tty = ttyname(STDIN_FILENO);
 
 	if (tty == nullptr) {
-		assert(errno == ENOTTY);
+		if (errno != ENOTTY) {
+			fprintf(stderr, "%s: %s\n", argv0, strerror(errno));
+			return ExitFailure;
+		}
 		printf("not a tty\n");
-		return 1;
+		return ExitNotTty;
 	}
 	printf("%s\n", tty);
 
-	return EXIT_SUCCESS;
+	return ExitSuccess;
 }
